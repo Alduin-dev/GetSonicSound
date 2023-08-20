c01b2107cfbc7a47c1e3be9ccb3a3670150c1b7c
Alduin <urielcabadat@hotmail.com>
2023-08-20 11:16:33 -0500

    Solutions Fixes Convertions

--- app.py
+++ app.py
@@ -1,39 +1,59 @@
-from pytube import Playlist
+from pytube import YouTube
 from pydub import AudioSegment
 import os
 import requests
 import io
-from tqdm import tqdm
+import re
 
-# Ingresa la URL para descargar el playlist
-playlist_url = 'https://www.youtube.com/playlist?list=PLHRfWmB-cTz-fopGK4LxferxnVKokgglm'
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage = (bytes_downloaded / total_size) * 100
+    print(f"Descargando... {percentage:.2f}% completado", end='\r')
 
-playlist = Playlist(playlist_url)
+# Ingresa la URL del video
+url = ''
 
-mp3_folder = 'playlistMusic'
+is_playlist = 'playlist' in url
+
+if is_playlist:
+    from pytube import Playlist
+    playlist = Playlist(url)
+else:
+    playlist = [url]
+
+mp3_folder = 'sounds'
 
 if not os.path.exists(mp3_folder):
     os.makedirs(mp3_folder)
 
-for video in playlist.videos:
-    print(f'Descargando: {video.title}')
-    
+for video_url in playlist:
+    if is_playlist:
+        video = YouTube(video_url)
+    else:
+        video = YouTube(video_url, on_progress_callback=on_progress)
+
+    print(f'Convirtiendo a MP3: {video.title}')
+
     audio_stream = video.streams.filter(only_audio=True).first()
-    
+
     response = requests.get(audio_stream.url, stream=True)
     total_size = int(response.headers.get('content-length', 0))
-    
-    mp3_path = os.path.join(mp3_folder, f'{video.title}.mp3')
-    
-    with open(mp3_path, 'wb') as f, tqdm(
-        desc=video.title,
-        total=total_size,
-        unit='B',
-        unit_scale=True,
-        unit_divisor=1024,
-    ) as bar:
-        for data in response.iter_content(chunk_size=1024):
-            f.write(data)
-            bar.update(len(data))
 
-print('Descarga completada.')
+    audio_content = b''
+    for chunk in response.iter_content(chunk_size=8192):
+        if chunk:
+            audio_content += chunk
+            percentage = len(audio_content) / total_size * 100
+            print(f"Descargando... {percentage:.2f}% completado", end='\r')
+
+    audio = AudioSegment.from_file(io.BytesIO(audio_content))
+    title = video.title if is_playlist else video.title + "_individual"
+    
+    title = re.sub(r'[\\/:"*?<>|]', '', title)
+    
+    mp3_path = os.path.join(mp3_folder, f'{title}.mp3')
+    audio.export(mp3_path, format='mp3')
+    print("Conversión completada:", title)
+
+print('Conversión a MP3 completada.')
